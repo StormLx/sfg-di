a463ac32d13a805e2f89ccfced5ed28edad41bc3
Alexandre <78553030+StormLx@users.noreply.github.com>
2021-06-11 19:36:41 +0200

    init project

---
+++ src/main/java/guru/sbdi/SbDiApplication.java
@@ -0,0 +1,13 @@
+package guru.sbdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbDiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbDiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/guru/sbdi/SbDiApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.sbdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
